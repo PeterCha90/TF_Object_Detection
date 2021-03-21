d393d8d41aa05dda7a649916f218a0f852ed79a3
Peter Cha <petercha90@gmail.com>
2021-03-21 18:53:40 +0900

    Update mobilenet_v2.py

--- Backbones/mobilenet_v2.py
+++ Backbones/mobilenet_v2.py
@@ -8,7 +8,6 @@ and retranslates old
 [mobilenet_v2_keras](https://github.com/JonathanCMitchell/mobilenet_v2_keras)
 keras code into tensorflow version 2.4.1 format. 
 """
-
 import os
 import warnings
 import numpy as np
@@ -18,7 +17,6 @@ import tensorflow.keras.backend as K
 import tensorflow.keras.layers as layers
 import tensorflow.keras.layers.Add as Add
 
-
 def inverted_res_block(inputs, expansion, out_channels,  
                        stride, block_id):
     in_channels = inputs.shape[-1]
@@ -26,30 +24,31 @@ def inverted_res_block(inputs, expansion, out_channels,
     if expansion != 1:
     # 1x1 conv2d, ReLU6 -> expansion
         x = layers.Conv2D(in_channels * expansion, 
-                        kernel_size=1, padding='same', 
-                        use_bias=False)(inputs)
+                          kernel_size=1, padding='same', 
+                          use_bias=False, activation=None,
+                          name='mobl_conv_expand' + block_id))(inputs)
         x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999,
-                                      name='bn%d_conv_bn_expand' %
-                                            block_id)(x)
-        x = tf.nn.relu6(x, name='conv_%d_relu' % block_id)
+                                      name='bn_conv_bn_expand '
+                                            + block_id)(x)
+        x = tf.nn.relu6(x, name='conv_relu' + block_id)
     
     # 3x3 depthwise s=s, ReLU6
         x = layers.DepthwiseConv2D(kernel_size=3, strides=stride,
                                    activation=None, use_bias=False,
                                    padding='same',
-                                   name='mobl%d_conv_depthwise' 
-                                         % block_id)(x)
+                                   name='mobl_conv_depthwise ' 
+                                         + block_id)(x)
     else:
         x = layers.DepthwiseConv2D(kernel_size=3, strides=stride,
                                    activation=None, use_bias=False,
                                    padding='same',
-                                   name='mobl%d_conv_depthsiwe' 
-                                         % block_id)(inputs)
+                                   name='mobl_conv_depthwise ' 
+                                         + block_id)(inputs)
 
     x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999,
-                                  name='bn%d_conv_depthwise' 
-                                        % block_id)(x)
-    x = tf.nn.relu6(x, name='conv_dw_%d_relu' % block_id)
+                                  name='bn_conv_depthwise ' 
+                                        + block_id)(x)
+    x = tf.nn.relu6(x, name='conv_dw_relu ' + block_id)
     
     # linear 1x1 conv2d
     x = layers.Conv2D(out_channels, kernel_size=1,
@@ -57,11 +56,11 @@ def inverted_res_block(inputs, expansion, out_channels,
                       activation=None,
                       name='bn%d_conv_bn_project' % block_id)(x)
     x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999,
-                                  name='bn%d_conv_bn_project' 
-                                  % block_id)(x)
+                                  name='bn_conv_bn_project ' 
+                                        + block_id)(x)
     
     if in_channels == out_channels and stride == 1:
-        return Add(name='res_connect_' + str(block_id))([inputs, x])
+        return Add(name='res_connect_' + block_id)([inputs, x])
 
     return x
 
@@ -86,7 +85,7 @@ def MobileNetV2(input_shape=None, input_tensor=None,
     else:
         if not K.is_keras_tensor(input_tensor):
             img_input = layers.Input(tensor=input_tensor,
-                              shape=input_shape)
+                                     shape=input_shape)
         else:
             img_input = input_tensor
     # model 
@@ -113,7 +112,8 @@ def MobileNetV2(input_shape=None, input_tensor=None,
                                    config[0], 
                                    config[1],
                                    stride, 
-                                   block_id=i+j)
+                                   block_id='P' + str(i) 
+                                            +'_'+ str(j))
 
     x = layers.Conv2D(1280, kernel_size=1, 
                       use_bias=False, name='Conv_last')(x)
